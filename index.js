3338467645c8dafc7d6ade41d4b7cd434f10114a
vinayBagde <vinaybagde0@gmail.com>
2025-02-03 15:41:36 +0530

    create index & new route

--- index.js
+++ index.js
@@ -1,6 +1,84 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+main()
+  .then((res) => {
+    console.log("connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+// let chat1 = new Chat({
+//   from: "neha",
+//   to: "priya",
+//   msg: "send me your exam sheets",
+//   created_at: new Date(),
+// });
+
+// chat1
+//   .save()
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+app.get("/", (req, res) => {
+  res.send("root is working");
+});
+
+//Index route
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index.ejs", { chats });
+});
+
+//new route
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+//create route
+app.post("/chats", (req, res) => {
+  let { from, msg, to } = req.body;
+  let newChat = new Chat({
+    from: from,
+    msg: msg,
+    to: to,
+    created_at: new Date(),
+  });
+
+  newChat()
+    .save()
+    .then((res) => {
+        console.log("chat was saved");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+    res.redirect("/chats");
+  
+});
 app.listen(8080, () => {
   console.log(`server is listening on port 8080}`);
 });
---
+++ init.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main()
+  .then((res) => {
+    console.log("connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [
+  {
+    from: "neha",
+    to: "priya",
+    msg: "send me your exam sheets",
+    created_at: new Date(),
+  },
+  {
+    from: "priya",
+    to: "alisa",
+    msg: "please give me notes",
+    created_at: new Date(),
+  },
+  {
+    from: "max",
+    to: "ray",
+    msg: "hey bro! what's going on",
+    created_at: new Date(),
+  },
+  {
+    from: "garry",
+    to: "taison",
+    msg: "let's go to the gym",
+    created_at: new Date(),
+  },
+];
+
+Chat.insertMany(allChats);
